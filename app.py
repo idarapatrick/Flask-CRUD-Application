216a1e5f19cf33ac610852c5b7be8ced97f95e1b
idarapatrick <essienidara67@gmail.com>
2024-12-10 07:01:44 +0200

    Added search & total transactions

--- app.py
+++ app.py
@@ -81,7 +81,28 @@ def delete_transaction(transaction_id):
     transactions = [transaction for transaction in transactions if transaction["id"] != transaction_id]
     return redirect(url_for("get_transactions"))
 
+ #Search Operation
+@app.route("/search", methods=["GET", "POST"])
+def search_transactions():
+    if request.method == "POST":
+        min_amount = float(request.form["min_amount"])
+        max_amount = float(request.form["max_amount"])
+        
+        filtered_transactions = [transaction for transaction in transactions if min_amount <= transaction["amount"] <= max_amount]
+        if not filtered_transactions:
+            return "No transactions were found"
+        
+        return render_template("transactions.html", transactions = filtered_transactions)
+    
+    # If it is a GET request
+    return render_template("search.html")
+                   
 
+# Total Balance Operation
+@app.route("/balance")
+def total_balance():
+    total = sum(transaction["amount"] for transaction in transactions)
+    return f"Total Balance: {total}"
 # Run the Flask app
 if __name__ == "__main__":
     app.run(debug=True)
